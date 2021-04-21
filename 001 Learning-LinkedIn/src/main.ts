77c2fa3609ce4951d3be1f7b25fd00e2c6537149
Gildder Guerrero <gilberet@hotmail.com>
2021-04-21 14:03:24 -0400

    Se realizaron los cambios del main.ts

--- 001 Learning-LinkedIn/src/main.ts
+++ 001 Learning-LinkedIn/src/main.ts
@@ -1,5 +1,20 @@
-import { CustomUser } from "./interfaces/Validation";
+import { CustomUser, StringValidator } from "./interfaces/Validation";
+
+let validators: { [s: string]: StringValidator } = {};
+
+// show whether each striong passed validator
+strings.forEach((s) => {
+  for (let name in validators) {
+    console.log(
+      `"{s}" - ${
+        validators[name].isAcceptable(s) ? "matches" : "does not match"
+      } ${name}`
+    );
+  }
+});
 
 console.log("Hola Mundo");
 
-const user: CustomUser;
+const user: CustomUser = {
+  name: "Sergio",
+};
